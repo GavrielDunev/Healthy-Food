bc13f7c8bfa271a8decf9ce9ce6107092d245875
Gavriel Dunev <dunev17@gmail.com>
2021-11-27 02:10:46 +0200

    Correction in current user profile views

--- src/main/java/com/example/healthyfood/service/impl/CurrentUserProfileViewsServiceImpl.java
+++ src/main/java/com/example/healthyfood/service/impl/CurrentUserProfileViewsServiceImpl.java
@@ -1,15 +1,9 @@
 package com.example.healthyfood.service.impl;
 
 import com.example.healthyfood.service.CurrentUserProfileViewsService;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
-
 
 @Service
 public class CurrentUserProfileViewsServiceImpl implements CurrentUserProfileViewsService {
@@ -20,8 +14,6 @@ public class CurrentUserProfileViewsServiceImpl implements CurrentUserProfileVie
     public void onRequest(HttpServletRequest httpServletRequest) {
 
         String requestedUrl = httpServletRequest.getRequestURL().toString();
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (requestedUrl.contains("/users/profile/")) {
             views++;
